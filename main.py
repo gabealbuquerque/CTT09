2ad8f25d4aaf54a18f2c829af699d213e5f7c537
Gabriel <gabriel.albuquerque@aluno.faculdadeimpacta.com.br>
2026-05-20 10:49:17 -0300

    adiciona multiplicacao

--- main.py
+++ main.py
@@ -13,3 +13,7 @@ def somar(a: int, b: int):
 @app.get("/multiplicar/{a}/{b}")
 def multiplicar(a: int, b: int):
     return {"resultado": a * b}
+
+@app.get("/multiplicar/{a}/{b}")
+def multiplicar(a: int, b: int):
+    return {"resultado": a * b}
